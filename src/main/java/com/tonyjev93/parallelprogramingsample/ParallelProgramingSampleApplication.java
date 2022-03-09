644386497067f3593f6cae4426f78d9b45f8ef33
JUN GYEWON <tonyjev93@gmail.com>
2022-03-09 10:07:35 +0900

    init

---
+++ src/main/java/com/tonyjev93/parallelprogramingsample/ParallelProgramingSampleApplication.java
@@ -0,0 +1,13 @@
+package com.tonyjev93.parallelprogramingsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelProgramingSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParallelProgramingSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tonyjev93/parallelprogramingsample/ParallelProgramingSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonyjev93.parallelprogramingsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParallelProgramingSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
